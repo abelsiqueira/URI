50b737002ae92b7189af040b4ba4aa0a9725fedd
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-01-08 14:12:27 -0200

    First commit

---
+++ newproblem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+problem=$1
+[[ "$#" -eq 1 ]] || { echo "1 argument required"; exit 1;}
+mkdir "$problem"
+touch "$problem"/STATUS
+echo $problem >> "$problem"/STATUS
+echo "---------------------------" >> "$problem"/STATUS
+cp templates/* "$problem"
