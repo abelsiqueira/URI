6571ac20fa108b1b413f64cdc13dbde59b811439
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-02-08 11:42:08 -0200

    Add 1517

--- 1517 - Apples/gentests.sh
+++ 1517 - Apples/gentests.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+n_tests=5
+for i in $(seq $n_tests)
+do
+  N=20
+  M=20
+  #n_apples=$[$RANDOM%10+5]
+  n_apples=1000
+  t=1
+  echo "$N $M $n_apples"
+  for k in $(seq $n_apples)
+  do
+    x=$[$RANDOM%$N+1]
+    y=$[$RANDOM%$M+1]
+    t=$[$t+$RANDOM%3+1]
+    echo "$x $y $t"
+  done
+  x=$[$RANDOM%$N+1]
+  y=$[$RANDOM%$M+1]
+  echo "$x $y"
+done
+echo "0 0 0"
--- 1517 - Apples/main.cpp
+++ 1517 - Apples/main.cpp
@@ -10,6 +10,10 @@ class Edge {
     Edge(int f = 0, int t = 0) : from(f), to(t) {}
     ~Edge() {}
     int from, to;
+
+    void print () const {
+      cout << '(' << from << ',' << to << ')' << endl;
+    }
 };
 
 class Graph {
@@ -22,11 +26,47 @@ class Graph {
       Edge edge(i,j);
       edges.push_back(edge);
     }
+
     int max_depth () const {
-      int n_edges = edges.length();
-      bool visited[n_edges];
-      for (int i = 0; i < n_edges; i++)
-        visited[i] = false;
+      int n_edges = edges.size();
+      int distance[nodes];
+      int father[nodes];
+
+      list<Edge>::const_iterator p = edges.begin();
+      list<Edge>::const_iterator p_end = edges.end();
+
+      for (int i = 0; i < nodes; i++) {
+        distance[i] = -100000;
+        father[i] = -1;
+      }
+      distance[0] = 0;
+
+      int max = 0;
+      for (int i = 0; i < nodes; i++) {
+        while (p != p_end && p->from == i) {
+          int j = p->to;
+          if (distance[j] < distance[i] + 1) {
+            distance[j] = distance[i] + 1;
+            father[j] = i;
+          }
+          if (distance[j] > max)
+            max = distance[j];
+          p++;
+        }
+      }
+
+      return max;
+
+    }
+
+    void print () const {
+      list<Edge>::const_iterator p = edges.begin();
+      list<Edge>::const_iterator p_end = edges.end();
+      cout << "Edges:" << endl;
+      while (p != p_end) {
+        p->print();
+        p++;
+      }
     }
 
     int nodes;
@@ -44,15 +84,18 @@ void process (int k) {
    * given time.
    * Nodes are the starting point and the fruit drop points.
    */
-  Graph graph; 
+  Graph graph(k+1); 
   for (int i = 0; i < k; i++) {
-    if (fabs(apx[i]-x)+fabs(apy[i]-y) < apt[i])
+    if (max(fabs(apx[i]-x),fabs(apy[i]-y)) <= apt[i])
       graph.add_edge(0,i+1);
+  }
+  for (int i = 0; i < k; i++) {
     for (int j = i+1; j < k; j++)
-      if (fabs(apx[i]-apx[j])+fabs(apy[i]-apy[j]) < apt[j]-apt[i])
+      if ( max(fabs(apx[i]-apx[j]), fabs(apy[i]-apy[j])) <= apt[j]-apt[i])
         graph.add_edge(i+1,j+1);
   }
 
+//  graph.print();
   cout << graph.max_depth() << endl;
 }
 
